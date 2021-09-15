b464ac6f3e397e9d88670d1e6a21067ba9740738
ykn113 <yukinosnj13@gmail.com>
2021-09-15 11:42:32 +0000

    [Update]debug

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,19 +8,17 @@ class RecipesController < ApplicationController
     # ↓ジャンル検索
     if params[:genre_id]
       @genre = @genres.find(params[:genre_id])
-      redirect_to users_path if @genre.recipes.select{|recipe| recipe.user == current_user}.empty?
-      @recipes = @genre.recipes.page(params[:page]).per(6)
+      @recipes = @genre.recipes.where(user_id: current_user.id).page(params[:page]).per(6)
     # ↓タグ検索
     elsif params[:tag_id]
       @tag = Tag.find(params[:tag_id])
-      redirect_to users_path if @tag.recipes.select{|recipe| recipe.user == current_user}.empty?
-      @recipes = @tag.recipes.page(params[:page]).per(6)
+      @recipes = @tag.recipes.where(user_id: current_user.id).page(params[:page]).per(6)
       @recipe_tag = @tag.tag_name
      # ↓すべて
     else
       @recipes = current_user.recipes.page(params[:page]).per(6).order(id: "DESC")
     end
-    @all_recipes_count = @recipes.count
+    @all_recipes_count = @recipes.total_count
   end
 
   def show
