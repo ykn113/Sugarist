fe04f2bd237d335fb216a475b4f9b0d72dd83f5c
ykn113 <yukinosnj13@gmail.com>
2021-09-18 09:08:07 +0000

    [Update]

--- app/models/recipe_tag.rb
+++ app/models/recipe_tag.rb
@@ -4,6 +4,4 @@ class RecipeTag < ApplicationRecord
 
   validates :recipe_id, presence: true
   validates :tag_id, presence: true
-
-  # add_index :recipe_tags, [:recipe_id, :tag_id], unique: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#  [["焼き菓子", "焼き菓子"], ["生菓子", "生菓子"], ["パン", "パン"]]
-
-Genre.create([
-  { name: "焼き菓子" },
-  { name: "生菓子" },
-  { name: "パン" },
-])
+Genre.create({ name: "焼き菓子"}) if !Genre.where(name: "焼き菓子").present?;
+Genre.create({ name: "生菓子"}) if !Genre.where(name: "生菓子").present?;
+Genre.create({ name: "パン"}) if !Genre.where(name: "パン").present?;
+Genre.create({ name: "和菓子"}) if !Genre.where(name: "和菓子").present?;
